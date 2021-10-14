80a2bda7e81666495302114bf2839eb229b8042d
David Johnston <djohnston@whispir.com>
2021-10-14 15:22:16 +1100

    Everything

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+
+    <rpl-button theme="primary">foo bar</rpl-button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import RplButton from '@dpc-sdp/ripple-button';
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld, 
+    RplButton,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
